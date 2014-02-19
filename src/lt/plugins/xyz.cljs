07b3b0c059fb8d8a2edf795a9aa90e2cd80fb08c
Mike Harris <mharris717@gmail.com>
2014-02-19 10:53:34 -0500

    CS

--- src/lt/plugins/xyz.cljs
+++ src/lt/plugins/xyz.cljs
@@ -1,8 +1,10 @@
 (ns lt.plugins.xyz)
 
+; errors if you add this to namespace
 '(:require-macros [lt.plugins.abc :refer [my-double-m]])
 
 (defn my-triple-f [x] (* x 3))
 (my-triple-f 3)
 
-(lt.plugins.abc/my-double-f 3)
+(defn ref-abc []
+  (lt.plugins.abc/my-double-f 3))
